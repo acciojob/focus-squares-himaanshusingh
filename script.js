0a94501eaf2f590ea0a4cb98f605f522df9bd001
Himanshu Singh <himanshusingh1k@gmail.com>
2025-09-29 21:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ squares.forEach((square) => {
     squares.forEach((sq) => {
       sq.style.backgroundColor = "#6F4E37";
     });
-    this.style.backgroundColor = "#E6E6FA";
+    this.style.backgroundColor = "#rgb(111, 78, 55)";
   });
   square.addEventListener("mouseleave", function () {
     squares.forEach((sq) => {
-      sq.style.backgroundColor = "#E6E6FA";
+      sq.style.backgroundColor = "#rgb(111, 78, 55)";
     });
   });
 });
